7f83b4833a076929c635f6ff82cada61842cb6e0
Niccolò Maltoni <nicco.mlt@gmail.com>
2020-06-14 21:14:51 +0200

    feat: add support to JAVA_EXEC declaration
    from version 7.0 (kaby76/Antlr4BuildTasks#4), Antlr4BuildTasks requires JAVA_EXEC environment variable instead of JAVA_HOME to work

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as core from '@actions/core'
+import * as path from 'path'
 import * as installer from './installer'
 
 const TOOL_NAME = 'antlr4'
@@ -7,6 +8,13 @@ const VERSION = '4.8'
 async function run(): Promise<void> {
   try {
     await installer.getAntlr(TOOL_NAME, VERSION)
+    const javaHome = process.env.JAVA_HOME
+    const javaExecVar = 'JAVA_EXEC'
+    if (!process.env[javaExecVar] && !!javaHome) {
+      const javaBin = path.join(javaHome, 'bin', 'java')
+      core.exportVariable(javaExecVar, javaBin)
+      core.info(`Exporting ${javaExecVar} variable with reference to java binary: ${javaBin}`)
+    }
   } catch (error) {
     core.setFailed(error)
   }
